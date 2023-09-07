1c09c4f3a8c03a8a9a7176441950e1c9dd2df949
Lilian Cristine Semann <Liliansemann2525@gmail.com>
2023-09-07 01:30:55 -0300

    06-09

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Card() {
+export function Card1() {
     return (
         <>
         <section className="h-fit w-fit p-3 border border-zinc-500 rounded-lg flex flex-col items-center gap-2">
@@ -11,6 +11,54 @@ export default function Card() {
                 className="rounded-md"
             />
             <p>Velha armada</p>
+        </section>
+        </>
+    )
+}
+
+export function Card2() {
+    return (
+        <>
+        <section className="h-fit w-fit p-3 border border-zinc-500 rounded-lg flex flex-col items-center gap-2">
+            <Image 
+                src={'/tortuga.jpg'} 
+                height={200} width={200} 
+                alt="Tartaruga fofa" 
+                className="rounded-md"
+            />
+            <p>Tartaruga fofa</p>
+        </section>
+        </>
+    )
+}
+
+export function Card3() {
+    return (
+        <>
+        <section className="h-fit w-fit p-3 border border-zinc-500 rounded-lg flex flex-col items-center gap-2">
+            <Image 
+                src={'/moto.jpg'} 
+                height={200} width={200} 
+                alt="Moto irada" 
+                className="rounded-md"
+            />
+            <p>Moto irada</p>
+        </section>
+        </>
+    )
+}
+
+export function Card4() {
+    return (
+        <>
+        <section className="h-fit w-fit p-3 border border-zinc-500 rounded-lg flex flex-col items-center gap-2">
+            <Image 
+                src={'/paraguaio.jpg'} 
+                height={200} width={200} 
+                alt="Paraguaio de lado" 
+                className="rounded-md"
+            />
+            <p>Paraguaio de lado</p>
         </section>
         </>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Card from "./components/cards";
+import { Card1, Card2, Card3, Card4 } from "./components/cards";
 import Footer from "./components/footer";
 import Header from "./components/header";
 
@@ -7,14 +7,12 @@ export default function Home() {
     <>
     <Header/>
     <section className="p-8 flex flex-col items-center gap-10">
-      <div className="flex gap-10"><Card/><Card/><Card/></div>
-      <div className="w-[65%] p-4 border border-zinc-500 rounded-lg flex flex-col gap-4">
-      <p>Can you hear, my heartbeat? Tired of feeling never enough. I close my eyes and tell myself that my dreams will come true. There'll be no more darkness when you believe in yourself, you're instopable.</p>
-      <p>E nessa loucura de dizer que não te quero Vou negando as aparências Disfarçando as evidências Mas pra que viver fingindo Se eu não posso enganar meu coração? Eu sei que te amo!
- Chega de mentiras De negar o meu desejo Eu te quero mais que tudo Eu preciso do seu beijo Eu entrego a minha vida Pra você fazer o que quiser de mim Só quero ouvir você dizer que sim!
- Diz que é verdade, que tem saudade Que ainda você pensa muito em mim Diz que é verdade, que tem saudade Que ainda você quer viver pra mim
-      </p>
+      <div className="flex gap-10">
+        <Card1/><Card2/><Card3/><Card4/>
       </div>
+      <main className="w-[65%] p-4 border border-zinc-500 rounded-lg flex flex-col gap-4">
+      
+      </main>
     </section>
     <Footer/>
     </>
