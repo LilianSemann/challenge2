99df42836f966b382dde8c678267ec2fe27a24ca
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2023-09-14 07:49:04 -0300

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
         <>
         <header className="bg-[#010107] text-white w-full h-24 flex justify-between gap-4 py-3 px-7 items-center text-lg fixed">
             <Link href={'/'} className="hover:scale-105 transition">My Very Real Company</Link>
-            <div className="flex gap-4">
+            <nav className="flex gap-4">
                 <Link href={'/about'} className="px-3 py-2 rounded-3xl hover:bg-white hover:text-black hover:font-medium hover:scale-105 transition">About</Link>
                 <Link href={'/'} className="px-3 py-2 rounded-3xl hover:bg-white hover:text-black hover:font-medium hover:scale-105 transition">Contact</Link>
-            </div>
+            </nav>
         </header>
         </>
     )
